a1eacff381032f4cdf13735a9d547432d6b6d7f9
noam003 <noam@bu.edu>
2025-02-20 18:29:21 -0500

    Updates so far... with whisper

---
+++ TextToResponse/txtToLLM.py
@@ -0,0 +1,15 @@
+import manualCheck
+import pandas as pd
+
+
+# import transcript
+with open("../SpeechToText/transcript.txt", "r") as file:
+    text = file.read()
+
+#manual check
+text = manualCheck.obviousMispellings(text)
+print(text)
+
+#feed to LLM now
+#still not sure how id feed this with a whole sentence
+professorsNames = {}
